df5f6f64045a8a73d22bd50d97b82f04e3635c71
Miguel Martinez-Alvarez <miguel.martinez@signal.uk.com>
2015-06-15 19:34:23 +0100

    Small Refactoring

--- src/ibmwatson/run.clj
+++ src/ibmwatson/run.clj
@@ -2,7 +2,6 @@
 	(:require [ibmwatson.news :as news]
 			  [ibmwatson.text :as text]))
 
-
 (def text "Clouded Leopards Born in Thailand Via Artificial Insemination. For only the second time, 
 		   a litter of clouded leopard cubs has been born as the result of an artificial insemination. 
 		   Pierre Comizzoli, reproductive physiologist at the Smithsonian Conservation Biology 
@@ -12,13 +11,13 @@
 
 		   All this happened at the White House, where the President Barack Obaba was given a speech.")
 
-(def query "IBM")
+(def query "Greece")
 
 (println "TEXT")
-(print (text.annotate text))
+(print (text/annotate text))
 
 
 (println "NEWS")
-(print (news.sample))
-(print (news.positives query))
-(print (news.negatives query))
+(print (news/sample))
+(print (news/positives query 0.9))
+(print (news/negatives query 0.9))
