1b0c64a8e222de81570679379f66a63cdea4ae99
Miguel Martinez-Alvarez <miguel.martinez@signal.uk.com>
2015-06-15 18:52:03 +0100

    Dependencies

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject ibmwatson "SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.6.0"]                 
+                 [org.clojure/tools.cli "0.3.1"]
+                 [clj-http "1.1.2"]
+                 [org.clojure/data.json "0.2.6"]])
