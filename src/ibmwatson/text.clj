fa680ef1dbcfdf2d9c5e548be8404dcd79f05a7f
Miguel Martinez-Alvarez <miguel.martinez@signal.uk.com>
2015-06-16 18:37:25 +0100

    Refactor

--- src/ibmwatson/text.clj
+++ src/ibmwatson/text.clj
@@ -1,50 +1,55 @@
 (ns ibmwatson.text
 	(:require [clj-http.client :as client]))  	
 
-;; TODO: Refactor
-
+(defn- text-call
+	[text endpoint params]
+	(let [root_url (str "http://access.alchemyapi.com/calls/text/" endpoint)]
+		(client/get root_url {:query-params params})))
 
 (defn entities
 	[text api_key]
-	(let [root_url "http://access.alchemyapi.com/calls/text/TextGetRankedNamedEntities"]
-		(client/get root_url {:query-params {"apikey" api_key
-										 "text" text
-										 "outputMode" "json"
-										 "disambiguated" 1}})))
+	(let [params {"apikey" api_key
+				  "text" text
+				  "outputMode" "json"
+				  "disambiguated" 1}]
+	(text-call text "TextGetRankedNamedEntities" params)))
 
 (defn sentiment
 	[text api_key]
-	(let [root_url "http://access.alchemyapi.com/calls/text/TextGetTextSentiment"]
-		(client/get root_url {:query-params {"apikey" api_key
-										 "text" text
-										 "outputMode" "json"}})))
+	(let [params {"apikey" api_key
+				  "text" text
+				  "outputMode" "json"
+				  "disambiguated" 1}]
+	(text-call text "TextGetTextSentiment" params)))
 
 (defn entity-level-sentiment
 	[text api_key]
-	(let [root_url "http://access.alchemyapi.com/calls/text/TextGetRankedNamedEntities"]
-		(client/get root_url {:query-params {"apikey" api_key
-										 "text" text
-										 "outputMode" "json"
-										 "disambiguated" 1
-										 "sentiment" 1}})))
+	(let [params {"apikey" api_key
+				  "text" text
+				  "outputMode" "json"
+				  "disambiguated" 1}]
+	(text-call text "TextGetRankedNamedEntities" params)))
 
 (defn relations
 	[text api_key]
-	(let [root_url "http://access.alchemyapi.com/calls/text/TextGetRelations"]
-		(client/get root_url {:query-params {"apikey" api_key
-										 "text" text
-										 "outputMode" "json"}})))
+	(let [params {"apikey" api_key
+				  "text" text
+				  "outputMode" "json"
+				  "disambiguated" 1}]
+	(text-call text "TextGetRelations" params)))
 
 (defn taxonomy
 	[text api_key]
-	(let [root_url "http://access.alchemyapi.com/calls/text/TextGetRankedTaxonomy"]
-		(client/get root_url {:query-params {"apikey" api_key
-										 "text" text
-										 "outputMode" "json"}})))
+	(let [params {"apikey" api_key
+				  "text" text
+				  "outputMode" "json"
+				  "disambiguated" 1}]
+	(text-call text "TextGetRankedTaxonomy" params)))
 
 (defn concepts
 	[text api_key]
-	(let [root_url "http://access.alchemyapi.com/calls/text/TextGetRankedConcepts"]
-		(client/get root_url {:query-params {"apikey" api_key
-										 "text" text
-										 "outputMode" "json"}})))
+	(let [params {"apikey" api_key
+				  "text" text
+				  "outputMode" "json"
+				  "disambiguated" 1}]
+	(text-call text "TextGetRankedConcepts" params)))
