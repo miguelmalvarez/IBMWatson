03811805fcd3140e48fc037da040498b6ca1c59c
Miguel Martinez-Alvarez <miguel.martinez@signal.uk.com>
2015-06-16 17:54:55 +0100

    Taxonomy and concepts calls

--- src/ibmwatson/run.clj
+++ src/ibmwatson/run.clj
@@ -5,22 +5,23 @@
 
 (def api_key (:api_key (edn/read-string (slurp "resources/config.edn"))))
 
-(def text "Clouded Leopards Born in Thailand Via Artificial Insemination. For only the second time, 
-		   a litter of clouded leopard cubs has been born as the result of an artificial insemination. 
-		   Pierre Comizzoli, reproductive physiologist at the Smithsonian Conservation Biology 
-		   Institute (SCBI), performed the artificial insemination in Thailand last March alongside 
-		   Paweena Thuwanut, a former JoGayle Howard Postdoctoral Fellow at SCBI, and Wanlaya Tipkantha, 
-		   a doctoral candidate at Chulalongkorn University, who also studied at SCBI.
-
-		   All this happened at the White House, where the President Barack Obama was given a speech.")
+(def text "Dick Costolo: Search for new Twitter CEO is legitimate
+		   Dick Costolo is stepping down as Twitter CEO on July 1 but that didn’t 
+		   stop him from publicly discussing the company onstage at Bloomberg’s 
+		   tech conference in San Francisco on Tuesday. He also dispelled the notion 
+		   that Twitter's board had already decided cofounder Jack Dorsey will be his 
+		   permanent successor.")
 
 (def query "Greece")
 
 (println "TEXT")
 (print (text/entities text api_key))
 (print (text/entity-level-sentiment text api_key))
+(print (text/relations text api_key))
+(print (text/taxonomy text api_key))
+(print (text/concepts text api_key))
 
 (println "NEWS")
-(print (news/sample api_key))
-(print (news/positives query 0.6 api_key))
-(print (news/negatives query 0.6 api_key))
+; (print (news/sample api_key))
+; (print (news/positives query 0.6 api_key))
+; (print (news/negatives query 0.6 api_key))
--- src/ibmwatson/text.clj
+++ src/ibmwatson/text.clj
@@ -1,6 +1,9 @@
 (ns ibmwatson.text
 	(:require [clj-http.client :as client]))  	
 
+;; TODO: Refactor
+
+
 (defn entities
 	[text api_key]
 	(let [root_url "http://access.alchemyapi.com/calls/text/TextGetRankedNamedEntities"]
@@ -9,14 +12,12 @@
 										 "outputMode" "json"
 										 "disambiguated" 1}})))
 
-
 (defn sentiment
 	[text api_key]
 	(let [root_url "http://access.alchemyapi.com/calls/text/TextGetTextSentiment"]
 		(client/get root_url {:query-params {"apikey" api_key
 										 "text" text
 										 "outputMode" "json"}})))
-
 
 (defn entity-level-sentiment
 	[text api_key]
@@ -26,3 +27,24 @@
 										 "outputMode" "json"
 										 "disambiguated" 1
 										 "sentiment" 1}})))
+
+(defn relations
+	[text api_key]
+	(let [root_url "http://access.alchemyapi.com/calls/text/TextGetRelations"]
+		(client/get root_url {:query-params {"apikey" api_key
+										 "text" text
+										 "outputMode" "json"}})))
+
+(defn taxonomy
+	[text api_key]
+	(let [root_url "http://access.alchemyapi.com/calls/text/TextGetRankedTaxonomy"]
+		(client/get root_url {:query-params {"apikey" api_key
+										 "text" text
+										 "outputMode" "json"}})))
+
+(defn concepts
+	[text api_key]
+	(let [root_url "http://access.alchemyapi.com/calls/text/TextGetRankedConcepts"]
+		(client/get root_url {:query-params {"apikey" api_key
+										 "text" text
+										 "outputMode" "json"}})))
